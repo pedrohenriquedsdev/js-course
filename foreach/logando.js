3061e99592b2d1aae8871d6ca3d2960073040da3
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-30 14:05:20 -0300

    Commit 1 - Path Foreach com Exercícios!

---
+++ foreach/logando.js
@@ -0,0 +1,3 @@
+const nomes = ['Pedro', 'Henrique', 'Allicia', 'Fernando'];
+
+const nomesLogados = nomes.forEach(nome => console.log(`Usuário ${nome.toUpperCase()} logado com sucesso!`));
---
+++ foreach/somando.js
@@ -0,0 +1,5 @@
+const numeros = [4, 6, 8, 10];
+let soma = 0;
+
+numeros.forEach(numero => soma += numero);
+console.log(soma);
