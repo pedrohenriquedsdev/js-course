db2a6e7d42a133c4a6fd2b9aa10d69b1db11ed28
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-11-03 09:05:01 -0300

    Commit 7 - Add to Filter path

---
+++ Array/Filter/nomes.js
@@ -0,0 +1,6 @@
+// Nomes para filtrar os que começam com "A"
+const nomes = ["Ana", "João", "Allicia", "Pedro", "Amanda"];
+
+const nomesComA = nomes.filter(n => n[0] === 'A');
+
+console.log(nomesComA);
