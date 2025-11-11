95f0ad023b171f5c01acfc272d38959f32d8123b
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-11-11 18:03:41 -0300

    Day 1 - JS UDEMY

---
+++ aula6/index.js
@@ -0,0 +1,16 @@
+let nome = 'João'; //Declaro e Inicializo
+nome = 'Pedro' //Inicializo
+let nome2; // Apenas Declaro
+
+console.log(nome, 'nasceu em 1894.')
+console.log(nome2);
+
+
+//Regras Importantes
+
+// Não podemos criar varáiveis que comecem com numeros
+// Variáveis precisam ter nomes que façam sentido
+// Sem espaços
+// Usamos camelCase e nao snake_case
+// Case-sensitive (Letras maiúsculas e minúsculas fazem diferença!)
+// Não podemos redeclarar variáveis com Let!
