8cc96c48acf6e0cdf04011ee8d8a96e3452887b3
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-11-13 19:48:36 -0300

    Day 2

--- aula9/js/script.js
+++ aula9/js/script.js
@@ -1,2 +1,4 @@
-alert('Olá, tudo bem?') // 
-const feliz = confirm('Está feliz?')
+alert('Olá que bom ter você aqui!'); //Undefined
+let nome = prompt('Digite seu nome:'); //String or Null
+let presenca = confirm('Pode confirmar que você esteve aqui?'); //True or False
+
